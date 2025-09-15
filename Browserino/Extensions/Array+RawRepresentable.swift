b6dc88aa06d5d68185f1b0d2709a3462a4e1f18c
Patrick Wyatt <pat@codeofhonor.com>
2025-09-15 10:24:59 -0700

    Add @retroactive to Array and Dictionary extensions

--- Browserino/Extensions/Array+RawRepresentable.swift
+++ Browserino/Extensions/Array+RawRepresentable.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-extension Array: RawRepresentable where Element: Codable {
+extension Array: @retroactive RawRepresentable where Element: Codable {
     public init?(rawValue: String) {
         guard let data = rawValue.data(using: .utf8),
               let result = try? JSONDecoder().decode([Element].self, from: data)
@@ -27,7 +27,7 @@ extension Array: RawRepresentable where Element: Codable {
     }
 }
 
-extension Dictionary: RawRepresentable where Key == String, Value: Codable {
+extension Dictionary: @retroactive RawRepresentable where Key == String, Value: Codable {
     public init?(rawValue: String) {
         guard let data = rawValue.data(using: .utf8),
               let result = try? JSONDecoder().decode(Self.self, from: data)
